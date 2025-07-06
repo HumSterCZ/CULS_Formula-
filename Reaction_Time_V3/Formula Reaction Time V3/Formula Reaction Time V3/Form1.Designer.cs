ad9000ad530fd31c64ff76c3be60b2a9595ece54
Tomáš Křeček <tomkrecek1@gmail.com>
2025-07-06 12:34:29 +0200

    Add files via upload

---
+++ Reaction_Time_V3/Formula Reaction Time V3/Formula Reaction Time V3/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Formula_Reaction_Time_V3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.start_sequence = new System.Windows.Forms.Timer(this.components);
+            this.random_timer = new System.Windows.Forms.Timer(this.components);
+            this.wait = new System.Windows.Forms.Timer(this.components);
+            this.winner = new System.Windows.Forms.Timer(this.components);
+            this.d1 = new System.Windows.Forms.Timer(this.components);
+            this.d2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(595, 320);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(718, 394);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox1.Font = new System.Drawing.Font("Bahnschrift", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.ForeColor = System.Drawing.Color.Gold;
+            this.textBox1.HideSelection = false;
+            this.textBox1.Location = new System.Drawing.Point(251, 387);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(210, 116);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = "5";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox2.Font = new System.Drawing.Font("Bahnschrift", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.ForeColor = System.Drawing.Color.Gold;
+            this.textBox2.HideSelection = false;
+            this.textBox2.Location = new System.Drawing.Point(1455, 387);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(210, 116);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TabStop = false;
+            this.textBox2.Text = "5";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Black;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox3.Font = new System.Drawing.Font("Bahnschrift", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox3.ForeColor = System.Drawing.Color.LightGray;
+            this.textBox3.HideSelection = false;
+            this.textBox3.Location = new System.Drawing.Point(251, 613);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(210, 116);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.TabStop = false;
+            this.textBox3.Text = "120";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.Black;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox4.Font = new System.Drawing.Font("Bahnschrift", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox4.ForeColor = System.Drawing.Color.LightGray;
+            this.textBox4.HideSelection = false;
+            this.textBox4.Location = new System.Drawing.Point(1455, 613);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(210, 116);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.TabStop = false;
+            this.textBox4.Text = "120";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.Black;
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox5.Font = new System.Drawing.Font("Bahnschrift", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox5.ForeColor = System.Drawing.Color.LightGray;
+            this.textBox5.HideSelection = false;
+            this.textBox5.Location = new System.Drawing.Point(167, 867);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(763, 116);
+            this.textBox5.TabIndex = 10;
+            this.textBox5.TabStop = false;
+            this.textBox5.Text = "Press button";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.Color.Black;
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox6.Font = new System.Drawing.Font("Bahnschrift", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox6.ForeColor = System.Drawing.Color.LightGray;
+            this.textBox6.HideSelection = false;
+            this.textBox6.Location = new System.Drawing.Point(984, 867);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(763, 116);
+            this.textBox6.TabIndex = 11;
+            this.textBox6.TabStop = false;
+            this.textBox6.Text = "Press button";
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.Color.Black;
+            this.textBox7.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox7.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox7.Font = new System.Drawing.Font("Bahnschrift", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox7.ForeColor = System.Drawing.Color.LightGray;
+            this.textBox7.HideSelection = false;
+            this.textBox7.Location = new System.Drawing.Point(167, 867);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(1568, 116);
+            this.textBox7.TabIndex = 12;
+            this.textBox7.TabStop = false;
+            this.textBox7.Text = "Game started";
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox7.Visible = false;
+            // 
+            // start_sequence
+            // 
+            this.start_sequence.Interval = 500;
+            this.start_sequence.Tick += new System.EventHandler(this.start_sequence_Tick);
+            // 
+            // random_timer
+            // 
+            this.random_timer.Interval = 1;
+            this.random_timer.Tick += new System.EventHandler(this.random_timer_Tick);
+            // 
+            // wait
+            // 
+            this.wait.Interval = 1000;
+            this.wait.Tick += new System.EventHandler(this.wait_Tick);
+            // 
+            // winner
+            // 
+            this.winner.Tick += new System.EventHandler(this.winner_Tick);
+            // 
+            // d1
+            // 
+            this.d1.Interval = 1000;
+            this.d1.Tick += new System.EventHandler(this.d1_Tick);
+            // 
+            // d2
+            // 
+            this.d2.Interval = 1000;
+            this.d2.Tick += new System.EventHandler(this.d2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1899, 995);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reaction time V3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Timer start_sequence;
+        private System.Windows.Forms.Timer random_timer;
+        private System.Windows.Forms.Timer wait;
+        private System.Windows.Forms.Timer winner;
+        private System.Windows.Forms.Timer d1;
+        private System.Windows.Forms.Timer d2;
+    }
+}
+
---
+++ Reaction_Time_V3/Formula Reaction Time V3/Formula Reaction Time V3/Form1.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+
+namespace Formula_Reaction_Time_V3
+{
+    public partial class Form1 : Form
+    {
+        clsResize _form_resize;
+        Random rnd = new Random();
+        Stopwatch timer = new Stopwatch();
+        public static Form1 f1Init;
+        public int rnd_time_tolerance_down = 50;
+        public int rnd_time_tolerance_up = 200;
+
+        int score1 = 0;
+        int score2 = 0;
+
+        int waittime = 0;
+        int winnertime = 0;
+
+        int debounce_time1 = 0;
+        int debounce_time2 = 0;
+        bool unlock_button1 = true;
+        bool unlock_button2 = true;
+
+        static int starting_time_sequence = 0;
+        static int rnd_time = 0;
+        static int random_timer_ticks = 0;
+        bool ready1 = false;
+        bool ready2 = false;
+        bool block = false;
+        bool game = false;
+        bool game_OK = false;
+        int time1 = 0;
+        int time2 = 0;
+        bool player1_finish = false;
+        bool player2_finish = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            f1Init = this;
+            _form_resize = new clsResize(this);
+            this.Load += _Load;
+            this.Resize += _Resize;
+            this.BackgroundImage = pictureBox1.BackgroundImage = Formula_Reaction_Time_V3.Properties.Resources.p;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Reset_Game();
+            MegaReset();
+        }
+
+        private void MegaReset()
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            score1 = 0;
+            score2 = 0;
+        }
+
+        private void Reset_Game()
+        {           
+            textBox3.Text = "0";
+            textBox4.Text = "0";
+            textBox5.Text = "Ready?";
+            textBox6.Text = "Ready?";
+            pictureBox1.BackgroundImage = Formula_Reaction_Time_V3.Properties.Resources.grey;
+            textBox7.Visible = false;
+            starting_time_sequence = 0;
+            rnd_time = 0;
+            random_timer_ticks = 0;
+            ready1 = false;
+            ready2 = false;
+            block = false;
+            game = false;
+            game_OK = false;
+            time1 = 0;
+            time2 = 0;
+            player1_finish = false;
+            player2_finish = false;
+            start_sequence.Stop();
+            random_timer.Stop();
+            wait.Stop();
+            winner.Stop();
+            d1.Stop();
+            d2.Stop();
+            debounce_time1 = 0;
+            debounce_time2 = 0;
+            unlock_button1 = true;
+            unlock_button2 = true;
+            textBox7.Visible = false;
+            textBox7.Text = "Game started";
+            timer.Stop();
+            timer.Reset();
+        }
+
+
+
+        #region Form Resizing - změna velikosti okna        
+        private void _Load(object sender, EventArgs e)
+        {
+            _form_resize._get_initial_size();
+        }
+
+        private void _Resize(object sender, EventArgs e)
+        {
+            _form_resize._resize();
+        }
+        #endregion       
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.R)
+            {
+                Reset_Game();
+                MegaReset();
+            }
+            if (!block) 
+            {
+                if (e.KeyCode == Keys.F11 && unlock_button1)
+                {
+                    d1.Start();
+                    unlock_button1 = false;
+
+                    if (game)
+                    {
+                        if (game_OK)
+                        {
+                            time1 = (int)timer.ElapsedMilliseconds;
+                            player1_finish = true;
+                            textBox5.Text = "OK";
+                            textBox7.Visible = false;
+                            textBox3.Text = time1.ToString();
+                        }
+                        else
+                        {
+                            timer.Stop();
+                            start_sequence.Stop();
+                            random_timer.Stop();
+                            block = true;
+                            pictureBox1.BackgroundImage = Formula_Reaction_Time_V3.Properties.Resources.orange;
+                            textBox5.Text = "Fail";
+                            if (score1 > 0)
+                            {
+                                score1--;
+                            }
+                            textBox1.Text = score1.ToString();
+                            textBox2.Text = score2.ToString();
+                            wait.Start();
+                        }
+                    }
+                    else if (!ready1)
+                    {
+                        ready1 = true;
+                        textBox5.Text = "READY";
+                    }
+                }
+                if (e.KeyCode == Keys.F12 && unlock_button2)
+                {
+                    d2.Start();
+                    unlock_button2 = false;
+
+                    if (game)
+                    {
+                        if (game_OK)
+                        {
+                            time2 = (int)timer.ElapsedMilliseconds;
+                            player2_finish = true;
+                            textBox6.Text = "OK";
+                            textBox7.Visible = false;
+                            textBox4.Text = time2.ToString();
+                        }
+                        else
+                        {
+                            timer.Stop();
+                            start_sequence.Stop();
+                            random_timer.Stop();
+                            block = true;
+                            pictureBox1.BackgroundImage = Formula_Reaction_Time_V3.Properties.Resources.orange;
+                            textBox6.Text = "Fail";
+                            if (score2 > 0)
+                            {
+                                score2--;
+                            }
+                            textBox1.Text = score1.ToString();
+                            textBox2.Text = score2.ToString();
+                            wait.Start();
+                        }
+                    }
+                    else if (!ready2)
+                    {
+                        ready2 = true;
+                        textBox6.Text = "READY";
+                    }                   
+                }
+
+                if (ready1 && ready2 && !game)
+                {
+                    block = true;
+                    game = true;
+                    textBox7.Visible = true;
+                    pictureBox1.BackgroundImage = Formula_Reaction_Time_V3.Properties.Resources.green;
+                    start_sequence.Start();             
+                }
+
+                if (player1_finish && player2_finish)
+                {
+                    if (time1 < time2)
+                    {
+                        score1++;
+                    }
+                    else if (time1 > time2)
+                    {
+                        score2++;
+                    }
+                    else
+                    {
+                        score1++;
+                        score2++;
+                    }
+                    timer.Stop();
+                    block = true;
+                    textBox1.Text = score1.ToString();
+                    textBox2.Text = score2.ToString();
+                    if (score1 == 5 && score2 != 5)
+                    {
+                        textBox5.Text = "WINNER";
+                        winner.Start();
+                    }
+                    else if (score2 == 5 && score1 != 5)
+                    {
+                        textBox6.Text = "WINNER";
+                        winner.Start();
+                    }
+                    else
+                    {
+                        wait.Start();
+                    }                  
+                }
+            }
+        }
+
+        private void start_sequence_Tick(object sender, EventArgs e)
+        {
+            starting_time_sequence += 1;
+            if (starting_time_sequence == 1)
+            {
+                pictureBox1.BackgroundImage = Formula_Reaction_Time_V3.Properties.Resources._1;
+            }
+            if (starting_time_sequence == 2)
+            {
+                pictureBox1.BackgroundImage = Formula_Reaction_Time_V3.Properties.Resources._2;
+            }
+            if (starting_time_sequence == 3)
+            {
+                pictureBox1.BackgroundImage = Formula_Reaction_Time_V3.Properties.Resources._3;
+                block = false;
+                textBox6.Text = "";
+                textBox5.Text = "";
+                textBox7.Visible = false;
+            }
+            if (starting_time_sequence == 4)
+            {
+                pictureBox1.BackgroundImage = Formula_Reaction_Time_V3.Properties.Resources._4;
+            }
+            if (starting_time_sequence == 5)
+            {
+                pictureBox1.BackgroundImage = Formula_Reaction_Time_V3.Properties.Resources.red;              
+                starting_time_sequence = 0;
+                rnd_time = rnd.Next(rnd_time_tolerance_down, rnd_time_tolerance_up);
+                random_timer.Start();
+                start_sequence.Stop();
+            }
+        }
+
+        private void random_timer_Tick(object sender, EventArgs e)
+        {
+            random_timer_ticks += 1;
+            if (random_timer_ticks == rnd_time)
+            {
+                random_timer_ticks = 0;
+                pictureBox1.BackgroundImage = Formula_Reaction_Time_V3.Properties.Resources.grey;
+                timer.Start();
+                random_timer.Stop();
+                textBox7.Text = "Press button";
+                textBox7.Visible = true;
+                game_OK = true;
+            }
+        }
+
+        private void wait_Tick(object sender, EventArgs e)
+        {
+            waittime++;
+            if (waittime == 3)
+            {
+                block = true;
+                waittime = 0;
+                wait.Stop();
+                Reset_Game();
+            }
+        }
+
+        private void winner_Tick(object sender, EventArgs e)
+        {
+            winnertime++;
+            if (winnertime % 2 == 0)
+            {
+                pictureBox1.BackgroundImage = Formula_Reaction_Time_V3.Properties.Resources.green;
+            }
+            else
+            {
+                pictureBox1.BackgroundImage = Formula_Reaction_Time_V3.Properties.Resources.grey;
+            }
+            if (winnertime == 50)
+            {
+                block = true;
+                winnertime = 0;
+                winner.Stop();
+                Reset_Game();
+                MegaReset();
+            }
+        }
+
+        private void d1_Tick(object sender, EventArgs e)
+        {           
+            debounce_time1++;   
+
+            if (debounce_time1 == 2)
+            {
+                debounce_time1 = 0;
+                unlock_button1 = true;
+                d1.Stop();
+            }
+        }
+
+        private void d2_Tick(object sender, EventArgs e)
+        {
+            debounce_time2++;
+
+            if (debounce_time2 == 2)
+            {
+                debounce_time2 = 0;
+                unlock_button2 = true;
+                d2.Stop();
+            }
+        }
+    }
+}
